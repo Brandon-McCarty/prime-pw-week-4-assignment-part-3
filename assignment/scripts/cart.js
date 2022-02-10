5f662c3d64bd8040b9f545f5a0639ab0041d4531
Brandon McCarty <brandon.m.mccarty12@gmail.com>
2022-02-10 15:35:39 -0600

    initial commit

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,3 +1,13 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
+let basket = [];
+
+function addItem(item, array){
+    console.log('in addItem');
+    array.push(item);
+    return true;
+}
+console.log(`Basket is ${basket}`);
+console.log(`Adding cup -- should be true: `, addItem('cup', basket));
+console.log(`Basket is now ${basket}`);
