d2cd803ff3248ce4adbbfd39b92e0efe51571495
Brandon McCarty <brandon.m.mccarty12@gmail.com>
2022-02-10 16:38:53 -0600

    listItems complete

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -10,4 +10,23 @@ function addItem(item, array){
 }
 console.log(`Basket is ${basket}`);
 console.log(`Adding cup -- should be true: `, addItem('cup', basket));
+console.log(`Adding tack -- should be true: `, addItem('tack', basket));
 console.log(`Basket is now ${basket}`);
+
+function listItems(array){
+    console.log('in listItems');
+       return array;
+}
+console.log(basket);
+console.log(`The items in basket are: `, listItems(basket));
+
+function empty(array){
+    console.log('in empty');
+    for (let i = 0; i < array.length; i++){
+        array.pop(i);
+        return true;
+    }
+}
+
+
+console.log('-----Testing Functions-----');
