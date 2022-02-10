c7d8ae12f1b5acd4baa58fb013b297bcde4cb03d
Brandon McCarty <brandon.m.mccarty12@gmail.com>
2022-02-10 17:08:17 -0600

    starting stretch goals

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -31,7 +31,7 @@ function empty(array){
 //         array.pop();
 //     }
 //     return true;
-// }
+// } -- This one only removes one item from the array
 
 console.log(`Removing items -- should be true: `, empty(basket));
 console.log(`The items in basket are: `, listItems(basket));
@@ -45,3 +45,23 @@ console.log(`Basket is now ${basket}`);
 console.log(`The items in basket are: `, listItems(basket));
 console.log(`Removing items -- should be true: `, empty(basket));
 console.log(`The items in basket are: `, listItems(basket));
+
+console.log('-----Stretch Goals-----');
+const maxItems = 5;
+console.log(`Adding juice -- should be true: `, addItem('juice', basket));
+console.log(`Adding sandwich -- should be true: `, addItem('sandwich', basket));
+console.log(`Adding napkins -- should be true: `, addItem('napkins', basket));
+console.log(`Adding cup -- should be true: `, addItem('cup', basket));
+console.log(`Adding tack -- should be true: `, addItem('tack', basket));
+console.log(`The items in basket are: `, listItems(basket));
+
+
+function isFull(array){
+    if (array.length < maxItems){
+        return false;
+    }
+    return true;
+}
+
+console.log(`Basket is full (Should be true): `, isFull(basket));
+
